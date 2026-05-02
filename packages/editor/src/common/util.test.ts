40317c5eea9eef98258274932f8ac129e62268ac
lovebird <lovebirdsx@gmail.com>
2026-05-02 20:25:03 +0800

    去掉无用代码

--- packages/editor/src/common/util.test.ts
+++
@@ -1,8 +0,0 @@
-import * as assert from 'assert';
-import { getFileModifiedTime } from './util';
-
-suite('Util', () => {
-	test('get modify time', () => {
-		assert.ok(getFileModifiedTime(__filename) > 0);
-	});
-});
--- packages/editor/src/common/util.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'fs';
-
-export function getFileModifiedTime(filePath: string): number {
-	return fs.statSync(filePath).mtimeMs;
-}
